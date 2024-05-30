d930fdfe869e9da9225efe02c84052283c8088cd
Cifer-ti <draculemihawk232@gmail.com>
2024-05-30 18:50:51 +0100

    Add gitignore file

---
+++ vaccation-tracking-system/src/main/java/com/vts/vaccation_tracking_system/PruebaController.java
@@ -0,0 +1,11 @@
+package com.vts.vaccation_tracking_system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Tcontroller {
+
+    @GetMapping("/grim")
+    public String Grim
+}
